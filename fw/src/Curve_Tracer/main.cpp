94a1e0a539aa6a62994f5015eadd10b98adbb923
royy-mor <roymor.102@gmail.com>
2023-09-16 12:03:27 -0500

    Updated main w/ comm capability w/ Eclipse 0.3

--- fw/src/Curve_Tracer/main.cpp
+++ fw/src/Curve_Tracer/main.cpp
@@ -1,9 +1,9 @@
 /**
  * @file        main.cpp
- * @author      Matthew Yu (matthewjkyu@gmail.com)
- * @brief       Controls the IV Curve Tracer and performs PV measurements.
- * @version     0.2.0
- * @date        2022-10-25
+ * @author      Matthew Yu (matthewjkyu@gmail.com) & Roy Mor (roymor.102@gmail.com)
+ * @brief       Controls the IV Curve Tracer and performs PV measurements, able to communicate with Eclipse Simulator for custom parameters.
+ * @version     0.3.0
+ * @date        2022-12-13
  * @copyright   Copyright (c) 2022
  * @note        Use _DEBUG_TUNING flag to perform manual tuning.
  *              Default baud rate is 115200 baud.
@@ -13,12 +13,17 @@
  */
 
 #include "mbed.h"
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
 
 // #define _DEBUG_TUNING_ 1
 #define MAJOR_VERSION 0
 #define MINOR_VERSION 2
 #define PATCH_VERSION 0
-#define BAUD_RATE 115200
+#define BAUD_RATE 57600
 
 /** Peripherals. */
 static DigitalOut led_heartbeat(D1);
@@ -29,6 +34,12 @@ static AnalogOut dac_control(A3);
 static CAN can(D10, D2);
 static CANMessage msg;
 static BufferedSerial serial_port(USBTX, USBRX, BAUD_RATE);
+
+float GATE_OFF = 0.00;
+float GATE_ON = 1.00;
+float GATE_STEP = 0.001;
+uint8_t ITERATIONS = 25;
+uint32_t SETTLING_TIME_US = 0;
 
 /** Tickers. */
 static LowPowerTicker tick_heartbeat;
@@ -50,8 +61,8 @@ enum EncodingScheme {
 
 float cal_dac_control(float in) {
     // TODO: 10/25/22 recheck dac control calibration.
-    // const float slope = 9.9539;
-    // const float intercept = 0.0583;
+    const float slope = 9.9539;
+    const float intercept = 0.0583;
     return in;// * slope + intercept;
 }
 
@@ -77,11 +88,56 @@ float cal_sen_curr(float in, int num_iterations) {
     return 8.1169 * in / num_iterations + 0.014;
 }
 
+bool modeValid(enum Mode mode) {
+    return mode==0||mode==1||mode==2;
+}
+
+bool gatesValid(float lowGate, float highGate, float gateStep){
+    bool lowValid = lowGate >=0 && lowGate <=1 && lowGate < highGate;
+    bool highValid = highGate >=0 && highGate <=1 && lowGate < highGate;
+    bool stepValid = gateStep <= .1 && gateStep >= 0.001;
+    return lowValid && highValid && stepValid;
+}
+
+bool iterationsValid(std::uint8_t iterations) {
+    return iterations >= 1 && iterations <= 100;
+}
+
+bool settleTimeValid(std::uint32_t settlingTime) {
+    return settlingTime >= 1000 && settlingTime <= 100000;
+}
+
+void readBuffer(char * buffer, std::size_t BUFFER_SIZE) {
+    uint8_t idx = 0;
+    do {
+        //printf("\r\n");
+        uint8_t prev_idx = idx;
+        idx += serial_port.read(buffer + idx, BUFFER_SIZE);
+        //printf("%s", &buffer[prev_idx]);
+    } while (buffer[idx - 1] != '\n');
+}
+
+void okHandshake(char * buffer, std::size_t BUFFER_SIZE) {
+    bool ok = false;
+    while (!ok){
+        memset(buffer, '\0', BUFFER_SIZE);
+        readBuffer(buffer, BUFFER_SIZE);
+        if(strncmp(buffer, "OK\r\n", 4)==0) {
+            ok = true;
+            printf("OK_RECEIVED\r\n");
+        } else {
+            ok = false;
+        }
+    }
+}
+
 int main() {
+    serial_port.set_format(8, BufferedSerial::Even, 1);
+
     tick_heartbeat.attach(&heartbeat, 500ms);
     dac_control = 0.0;
 
-    printf("v%d.%d.%d\r\n", MAJOR_VERSION, MINOR_VERSION, PATCH_VERSION);
+    printf("v%d.%d.%d\n", MAJOR_VERSION, MINOR_VERSION, PATCH_VERSION);
 
 #ifdef _DEBUG_TUNING_
     /**
@@ -93,6 +149,7 @@ int main() {
      */
     printf("DEBUG MODE\r\n");
     enum Mode mode = CELL;
+
     while (1) {
         // Test duration one way: 6.275 seconds
         // 251 steps
@@ -100,7 +157,7 @@ int main() {
         // 25 substeps per step
         // 1 ms per substep
         #define GATE_OFF                0.25
-        #define GATE_ON                 0.50
+        #define GATE_ON                 0.75
         #define GATE_STEP               0.001
         #define SETTLING_TIME_US        1000 // us
         #define ITERATIONS              25
@@ -163,19 +220,15 @@ int main() {
     }
 #else
     /* Measurement mode. */
-    printf("MEASUREMENT MODE\r\n");
+    printf("MEASUREMENT MODE\n");
 
     /* Setup buffer and serial port. */
     #define BUFFER_SIZE 100
     char buffer[BUFFER_SIZE] = { 0 };
-    serial_port.set_format(8, BufferedSerial::Even, 1);
+
+    // char buffer2[BUFFER_SIZE] = { 0 };
 
     while (1) {
-        float GATE_OFF = 0.00;
-        float GATE_ON = 1.00;
-        float GATE_STEP = 0.001;
-        uint8_t ITERATIONS = 1;
-        uint32_t SETTLING_TIME_US = 0;
         enum Mode mode = CELL;
         enum EncodingScheme scheme = VERBOSE;
         bool valid_conf = false;
@@ -183,14 +236,39 @@ int main() {
 
         printf("READY_FOR_TRANSMISSION\r\n");
 
-        /* Read handshake message. */
-        while (true) {
-            serial_port.read(buffer, BUFFER_SIZE);
-            if (false) { // TODO: check for handshake message "OK"
-                printf("BEGIN_TRANSMISSION\r\n");
-                memset(buffer, '\0', BUFFER_SIZE);
-                break;
-            }
+        bool baudSet = false;
+        
+        while (!baudSet) {
+
+            okHandshake(buffer, BUFFER_SIZE);
+
+            printf("BEGIN_TRANSMISSION\r\n");
+            memset(buffer, '\0', BUFFER_SIZE);
+            ready = true;
+            baudSet = true;
+            break;
+
+            // memset(buffer, '\0', BUFFER_SIZE);
+            // printf("READY_FOR_BAUDRATE\r\n");
+            // readBuffer(buffer, BUFFER_SIZE);
+            // int newBaudRate = atoi(buffer); 
+            // printf("%d\r\n", newBaudRate);
+            // bool baudRateMismatch = newBaudRate!=BAUD_RATE;
+            // printf("%d\r\n", baudRateMismatch);
+
+            // if (baudRateMismatch) {
+            //     printf("SWAPPING_BAUDRATE\r\n");
+            //     wait_us(100000);
+            //     serial_port.close();
+            //     static BufferedSerial serial_port(USBTX, USBRX, newBaudRate);
+            //     // serial_port.set_baud(newBaudRate);  
+                
+            // } else if(!baudRateMismatch) {
+            
+            // }
+
+
+        
         }
 
         /**
@@ -199,19 +277,37 @@ int main() {
          */
 
         /* Get PV type */
-        serial_port.read(buffer, BUFFER_SIZE);
+    
+        readBuffer(buffer, BUFFER_SIZE);    
+        printf("%s", buffer);
         sscanf(buffer,
-            "type=%i,sr=[%f,%f,%f],ni=%i,st_ms=%i,enc=%i",
+            "type=%i,sr=[%f,%f,%f],ni=%i,st_ms=%i,enc=%i\r\n",
             &mode, &GATE_OFF, &GATE_ON, &GATE_STEP,
             &ITERATIONS, &SETTLING_TIME_US, &scheme);
         memset(buffer, '\0', BUFFER_SIZE);
 
+        printf("config received\r\n");
+        SETTLING_TIME_US*=1000;
         /* TODO: check for valid results */
+        if(modeValid(mode)) printf("mode valid\r\n");
+        if(gatesValid(GATE_OFF,GATE_ON,GATE_STEP)) printf("gates valid\r\n");
+        if(iterationsValid(ITERATIONS)) printf("iterations valid\r\n");
+        if(settleTimeValid(SETTLING_TIME_US)) printf("Settle Time valid\r\n");
+        valid_conf = modeValid(mode) && gatesValid(GATE_OFF, GATE_ON, GATE_STEP) && iterationsValid(ITERATIONS) && settleTimeValid(SETTLING_TIME_US);
+        if(valid_conf){
+            printf("valid config\r\n");
+        } else {
+            printf("invalid config\r\n");
+        }
 
         if (valid_conf && ready) {
-            printf("BEGIN_SCAN\n");
+            printf("BEGIN_SCAN\r\n");
             /* Forwards. */
-            for (dac_control = GATE_OFF; dac_control <= GATE_ON; dac_control = dac_control + GATE_STEP) {
+            printf("forwards\r\n");
+            printf("%f %f %f\r\n", GATE_OFF,GATE_ON,GATE_STEP);
+            float currentGate;
+            for (currentGate = GATE_OFF; currentGate <= GATE_ON; currentGate = currentGate + GATE_STEP) {
+                dac_control = currentGate;
                 float meas_volt = 0.00;
                 float meas_curr = 0.00;
                 float meas_control = cal_dac_control(dac_control);
@@ -226,10 +322,24 @@ int main() {
                 meas_volt = cal_sen_volt(meas_volt, ITERATIONS, mode);
                 meas_curr = cal_sen_curr(meas_curr, ITERATIONS);
 
+                switch (scheme) {
+                    case VERBOSE:
+                        printf(
+                            "Gate (V): %f, VSense (V): %f, ISense (A): %f, V*I (W): %f\n",
+                            meas_control,
+                            meas_volt,
+                            meas_curr,
+                            meas_volt * meas_curr
+                        );
+                        break;
+                }
+
             }
 
             /* Backwards. */
-            for (dac_control = GATE_ON; dac_control >= GATE_OFF; dac_control = dac_control - GATE_STEP) {
+            printf("backwards\r\n");
+            for (currentGate = GATE_ON; currentGate >= GATE_OFF; currentGate = currentGate - GATE_STEP) {
+                dac_control = currentGate;
                 float meas_volt = 0.00;
                 float meas_curr = 0.00;
                 float meas_control = cal_dac_control(dac_control);
@@ -256,7 +366,71 @@ int main() {
                         break;
                 }
             }
-            printf("END_SCAN\n");
+
+            /* Forwards. */
+            printf("forwards\r\n");
+            for (currentGate = GATE_OFF; currentGate <= GATE_ON; currentGate = currentGate + GATE_STEP) {
+                dac_control = currentGate;
+                float meas_volt = 0.00;
+                float meas_curr = 0.00;
+                float meas_control = cal_dac_control(dac_control);
+
+                /* SW based sample averaging control. */
+                for (uint8_t i = 0; i < ITERATIONS; ++i) {
+                    wait_us(SETTLING_TIME_US);
+                    meas_volt += sen_voltage.read();
+                    meas_curr += sen_current.read();
+                }
+
+                meas_volt = cal_sen_volt(meas_volt, 25, mode);
+                meas_curr = cal_sen_curr(meas_curr, 25);
+
+                switch (scheme) {
+                    case VERBOSE:
+                        printf(
+                            "Gate (V): %f, VSense (V): %f, ISense (A): %f, V*I (W): %f\n",
+                            meas_control,
+                            meas_volt,
+                            meas_curr,
+                            meas_volt * meas_curr
+                        );
+                        break;
+                }
+
+            }
+
+            /* Backwards. */
+            printf("backwards\r\n");
+            for (currentGate = GATE_ON; currentGate >= GATE_OFF; currentGate = currentGate - GATE_STEP) {
+                dac_control = currentGate;
+                float meas_volt = 0.00;
+                float meas_curr = 0.00;
+                float meas_control = cal_dac_control(dac_control);
+
+                /* SW based sample averaging control. */
+                for (uint8_t i = 0; i < ITERATIONS; ++i) {
+                    wait_us(SETTLING_TIME_US);
+                    meas_volt += sen_voltage.read();
+                    meas_curr += sen_current.read();
+                }
+
+                meas_volt = cal_sen_volt(meas_volt, ITERATIONS, mode);
+                meas_curr = cal_sen_curr(meas_curr, ITERATIONS);
+
+                switch (scheme) {
+                    case VERBOSE:
+                        printf(
+                            "Gate (V): %f, VSense (V): %f, ISense (A): %f, V*I (W): %f\n",
+                            meas_control,
+                            meas_volt,
+                            meas_curr,
+                            meas_volt * meas_curr
+                        );
+                        break;
+                }
+            }
+
+            printf("END_SCAN\r\n");
         }
     }
 #endif
